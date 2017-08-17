82bd5a4e410ace946e0169e2385348d54ee8d110
emmanuelletaylormaher <emmanuelle.t.maher@gmail.com>
2017-08-16 22:39:20 -0500

    Declare and initialized a second variable requesting user input

--- input.py
+++ input.py
@@ -3,3 +3,9 @@ user = input("I am a Python. What is your name?:")
 
 #output a string and a variable value
 print("Hello, ", user)
+
+#initialize another variable with a user-specified value
+lang = input("Favorite programming lanuage? :")
+
+#output a string and a variable value
+print(lang, "Is", "Fun", sep = "*", end = "!\n")
