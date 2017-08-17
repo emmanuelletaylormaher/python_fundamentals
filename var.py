20ce02ee5d38ebeec0161cd56162c3ccb9264568
emmanuelletaylormaher <emmanuelle.t.maher@gmail.com>
2017-08-16 22:33:34 -0500

    Declared variables

---
+++ var.py
@@ -0,0 +1,15 @@
+#initialize a variable with an integer value.
+var = 8
+print(var)
+
+#assign a float value to the variable
+var = 3.142
+print(var)
+
+#assign a string value to the variable
+var = "Python!"
+print(var)
+
+#assign a boolean value to the variable
+var = True
+print(var)
